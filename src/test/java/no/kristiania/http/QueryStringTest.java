7e2f6cd8f00b7d2f863b2e852b70b73ad04ea571
venici2606 <venicia_p@live.no>
2020-09-25 16:38:42 +0200

    first test green and added 401 code test

--- src/test/java/no/kristiania/http/QueryStringTest.java
+++ src/test/java/no/kristiania/http/QueryStringTest.java
@@ -10,7 +10,12 @@ public class QueryStringTest {
     void shouldRetrieveStatusCode() {
         QueryString queryString = new QueryString("status=200"); //klasse som heter queryString med enkel eksempel
         assertEquals("200", queryString.getParameter("status")); // sjekke at jeg får 200 tilbake getParameter status altså verdien av status skal være 200
+    }
 
+    @Test
+    void shouldRetrieveStatusCode_401() {
+        QueryString queryString = new QueryString("status=401");
+        assertEquals("401", queryString.getParameter("status"));
     }
 
 }
