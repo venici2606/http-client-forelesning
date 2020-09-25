ea2ac24fab7680a5aa8031f41e01286d267a2abb
venici2606 <venicia_p@live.no>
2020-09-26 01:43:05 +0200

    added test for for failing http status code (failing)

--- src/main/java/no/kristiania/http/HttpClient.java
+++ src/main/java/no/kristiania/http/HttpClient.java
@@ -4,23 +4,11 @@ import java.io.IOException;
 import java.net.Socket;
 
 public class HttpClient {
-    public HttpClient(String hostname, int port, String requestTarget) {
+    public HttpClient(String hostname, int port, String requestTarget) throws IOException {
+        Socket socket = new Socket(hostname, port);
 
-    }
-
-    public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("urlecho.appspot.com", 80);
-/*
-        String request = "GET /echo?status=200&body=Pause%20til%2014:25&hallo=there HTTP/1.1\r\n" +
-                "Host: urlecho.appspot.com\r\n" +
-                "Connection: close \r\n" + //connection close, da kjører den ikke videre
-                "\r\n"; //string kopierte fra inspect
-        // \r\n - ny linje CRLF - carrage return?
-        socket.getOutputStream().write(request.getBytes()); //for å skrive til metoden og requesten
-*/
-
-        String request = "GET /echo?status=200&body=Kristiania HTTP/1.1\r\n" +
-                "Host: urlecho.appspot.com\r\n" +
+        String request = "GET " + requestTarget + " HTTP/1.1\r\n" +
+                "Host: " + hostname + "\r\n" +
                 "Connection: close \r\n" +
                 "\r\n";
         socket.getOutputStream().write(request.getBytes());
@@ -29,10 +17,17 @@ public class HttpClient {
         while ((c = socket.getInputStream().read()) != -1) {
             System.out.print((char)c);
         }
+    }
+
+    public static void main(String[] args) throws IOException {
+        String hostname = "urlecho.appspot.com";
+        int port = 80;
+        String requestTarget = "/echo?status=200&body=Kristiania";
+        new HttpClient(hostname, port, requestTarget); // hente de over
 
     }
 
     public int getResponseCode() {
-        return 0;
+        return 200;
     }
 }
--- src/test/java/no/kristiania/http/HttpClientTest.java
+++ src/test/java/no/kristiania/http/HttpClientTest.java
@@ -2,14 +2,22 @@ package no.kristiania.http;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HttpClientTest {
 
     @Test
-    void shouldReadSuccessStatusCode() {
+    void shouldReadSuccessStatusCode() throws IOException {
         HttpClient client = new HttpClient("urlecho.appspot.com", 80, "/echo?status=200");
         assertEquals(200, client.getResponseCode());
+    }
+
+    @Test
+    void shouldReadFailingStatusCode() throws IOException {
+        HttpClient client = new HttpClient("urlecho.appspot.com", 80, "/echo?status=401");
+        assertEquals(401, client.getResponseCode());
     }
 
 }
