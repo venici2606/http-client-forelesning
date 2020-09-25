ac1048b2a1e9b6e56e01118507c3c81e0350544f
venici2606 <venicia_p@live.no>
2020-09-25 16:19:26 +0200

    Initial failing test for QueryString

---
+++ src/test/java/no/kristiania/http/QueryStringTest.java
@@ -0,0 +1,16 @@
+package no.kristiania.http;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class QueryStringTest {
+
+    @Test
+    void shouldRetrieveStatusCode() {
+        QueryString queryString = new QueryString("status=200"); //klasse som heter queryString med enkel eksempel
+        assertEquals("200", queryString.getParameter("status")); // sjekke at jeg får 200 tilbake getParameter status altså verdien av status skal være 200
+
+    }
+
+}
