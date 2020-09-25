d2fcb1c743598412e5755d007b2ed409b8b46751
venici2606 <venicia_p@live.no>
2020-09-25 23:04:14 +0200

    Changes after code review

--- src/test/java/no/kristiania/http/QueryStringTest.java
+++ src/test/java/no/kristiania/http/QueryStringTest.java
@@ -6,13 +6,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class QueryStringTest {
-
+/*
     @Test
     void shouldRetrieveStatusCode() {
         QueryString queryString = new QueryString("status=200"); //klasse som heter queryString med enkel eksempel
         assertEquals("200", queryString.getParameter("status")); // sjekke at jeg får 200 tilbake getParameter status altså verdien av status skal være 200
     }
-
+*/
     @Test
     void shouldRetrieveStatusCode_401() {
         QueryString queryString = new QueryString("status=401");
